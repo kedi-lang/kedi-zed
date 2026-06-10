8c1536ffe80707674f8153440cb3261f6c047d51
Mert <62549656+fswair@users.noreply.github.com>
2026-06-11 00:55:21 +0300

    update rev

--- languages/kedi/highlights.scm
+++ languages/kedi/highlights.scm
@@ -62,14 +62,10 @@
 (model_directive
   value: (inline_python_expr) @embedded)
 
-(system_directive
-  value: (model_plain_value) @text.literal)
-
-(system_directive
-  value: (inline_python_expr) @embedded)
-
 (input_segment
   name: (identifier) @variable)
+
+(system_angle_segment) @variable
 
 (call_segment
   name: (identifier) @function)
